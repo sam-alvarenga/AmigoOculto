8c9c15d0f1bb674c4aa7f6051e2829d89347f9b6
Samantha <samantha.c.alvarenga@gmail.com>
2024-06-20 22:16:49 -0300

    finalizado a funcionalidade do btn add e iniciando btn reset

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
-let friend = [];
-const textFriends = document.querySelector('.friends');
+let friends = [];
+const txtFriends = document.querySelector('.friends-input');
+//Botões
 const btnAddFriends = document.querySelector('.add');
-const listParticipantes = document.querySelector('.prize-draw');
+const btnReset = document.querySelector('.reset');
+
+const listaParticipantes = document.querySelector('.prize-draw');
 const mensagemErro = document.querySelector('.mensagem-erro');
 
-btnAddFriends.addEventListener('click', (event) => {
+//Botão Adicionar Amigos
+btnAddFriends.addEventListener('click', () => {
     if (validateFriend()) {
-        addFriends();
+        addFriends()
+
     }
 });
-textFriends.addEventListener('click', () => {
-    textFriends.value = '';
+
+//
+txtFriends.addEventListener('click', () => {
+    txtFriends.value = '';
 })
 
 function addFriends() {
-    if (friend.includes(textFriends.value)) {
-        mensagemErro.textContent = `${textFriends.value} já está na lista! Adicione outro nome`;
+    if (friends.includes(txtFriends.value)) {
+        mensagemErro.textContent = `${txtFriends.value} já está na lista! Adicione outro nome`;
         return;
     }
-    friend.push(textFriends.value);
-    listParticipantes.textContent = friend.join(",");
-    textFriends.value = '';
+    friends.push(txtFriends.value);
+    listaParticipantes.textContent = friends.join(",");
+    txtFriends.value = '';
 
 }
 
-function validatefriend() {
-    if (textFriends.value.length === 0) {
+function validateFriend() {
+    if (txtFriends.value.length <= 3) {
         mensagemErro.textContent = "Erro - Insira um nome"
         return false
     }
     return true;
+
+}
+
+btnReiniciar()
+    //Botão Reiniciar
+function btnReiniciar() {
+    text.value = '';
+    friends = [];
+    listaParticipantes.innerHTML = '';
+    friends.innerHTML = '';
+
 
 }
