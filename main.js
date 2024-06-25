60c5075289a6c577eed88557fb726f9a03db1f4c
Samantha <samantha.c.alvarenga@gmail.com>
2024-06-25 18:27:19 -0300

    botão sortear e embaralhar sem funcionalidade corrigir

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let friends = [];
-const txtFriends = document.querySelector('.friends-input');
 //Botões
 const btnAddFriends = document.querySelector('.add');
+const btnPrize = document.querySelector('.prize');
 const btnReset = document.querySelector('.reset');
-const btnPrize  = document.querySelector('.prize')
 
+const txtFriends = document.querySelector('.friends-input');
 const listaParticipantes = document.querySelector('.prize-draw');
-const resultado = document.querySelector('.results');
+const sorteio = document.querySelector('.results');
 const mensagemErro = document.querySelector('.mensagem-erro');
+const i = listaParticipantes.lenght - 1;
 
-//Botão Adicionar Amigos
 btnAddFriends.addEventListener('click', () => {
     if (validateFriend()) {
         addFriends()
@@ -22,15 +22,23 @@ txtFriends.addEventListener('click', () => {
     txtFriends.value = '';
 })
 
+btnReset.addEventListener('click', () => {
+  reiniciar()
+
+});
+
 btnPrize.addEventListener('click', () => {
-    btnSortear()
+    sortear()
 })
 
+//Botão Adicionar Amigos
 function addFriends() {
     if (friends.includes(txtFriends.value)) {
         mensagemErro.textContent = `${txtFriends.value} já está na lista! Adicione outro nome`;
         return;
     }
+
+    LimparMsgErro()
     friends.push(txtFriends.value);
     listaParticipantes.textContent = friends.join(",");
     txtFriends.value = '';
@@ -46,14 +54,51 @@ function validateFriend() {
 
 }
 
-btnReiniciar()
-//Botão Reiniciar
-function btnReiniciar() {
-    btnReset.addEventListener('click', () => {
-        txtFriends.value = '';
-        friends = [];
-        listaParticipantes.innerHTML = '';
-
-    })
+function LimparMsgErro() {
+    // console.log(LimparMsgErro)
+    mensagemErro.textContent = ''
 }
 
+function mostrarErro(textoMensagem) {
+    mensagemErro.textContent = `ERRO: ${textoMensagem}`
+}
+
+
+//Botão Reiniciar
+
+function reiniciar() {
+    txtFriends.value = '';
+    friends = [];
+    listaParticipantes.innerHTML
+    sorteio.innerHTML = '';
+}
+
+//Embaralhamento
+
+function embaralhar(listaNomes) {
+    for (let indice = listaNomes.length; indice; indice--) {
+        let indiceAleatorio = Math.floor(Math.random() * indice)
+
+        [listaNomes[indice - 1], listaNomes[indiceAleatorio]] = [listaNomes[indiceAleatorio], listaNomes[indice - 1]];
+    }
+}
+
+//BOTÂO SORTEIO
+
+function sortear() {
+    if (txtFriends.length < 4) {
+        mostrarErro('Adicionar ao menos 4 amigos')
+        return;
+    }
+}
+
+embaralhar(friends);
+sorteio.innerHTML = '';
+for (let i = 0; i < txtFriends.length; i++) {
+    if (i == txtFriends.length - 1) {
+        sorteio.innerHTML = sorteio.innerHTML + '<li>' + friends[i] + '--->' + friends[0] + '</li>';
+    } else {
+        sorteio.innerHTML = sorteio.innerHTML + '<li>' + friends[i] + '--->' + friends[i + 1] + '</li>';
+
+    }
+}
